0d67078e3e18547ed8b7ace76dbfb701313d986c
Jelson <jelsonlanto25@gmail.com>
2025-09-12 05:46:14 +0800

    Add customer and user management views and controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Client\PropertyController;
+use App\Http\Controllers\Client\CustomerController;
+use App\Http\Controllers\Client\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -14,4 +16,6 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::prefix('client')->middleware(['auth:web'])->group(function() {
     Route::resource('properties', PropertyController::class);
+    Route::resource('customers', CustomerController::class)->only(['index','edit','update']);
+    Route::resource('users', UserController::class)->only(['index','edit','update']);
 });
