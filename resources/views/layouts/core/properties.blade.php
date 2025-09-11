ab0d348085fb4405910f2ce79d3115065a3eeb68
Jelson <jelsonlanto25@gmail.com>
2025-09-12 06:04:47 +0800

    Add migration for property name column

--- resources/views/layouts/core/properties.blade.php
+++ resources/views/layouts/core/properties.blade.php
@@ -1,4 +1,9 @@
 <!-- Featured Properties start -->
+@php
+    use App\Models\Property;
+    use Illuminate\Support\Facades\Storage;
+    $featuredProperties = Property::with(['units', 'galleries'])->latest()->take(5)->get();
+@endphp
 <div class="featured-properties content-area-16 slide-box-2">
     <div class="container">
         <!-- Main title -->
@@ -7,198 +12,46 @@
         </div>
     </div>
     <div class="slide-container">
-        <div class="wrapper">
-            <div class="card">
-                <div class="cardImage">
-                    <div class="property-box-4">
-                        <div class="property-photo">
-                            <img class="img-fluid w-100" src="https://storage.googleapis.com/theme-vessel-items/checking-sites/neer-html/HTML/main/img/properties/properties-1.jpg" alt="properties">
-                            <div class="tag">For Sale</div>
-                            <div class="plan-price"><sup>$</sup>600<span>/month</span></div>
-                        </div>
-                        <div class="detail">
-                            <div class="heading">
-                                <h3>
-                                    <a href="properties-details.html">Masons Villas</a>
-                                </h3>
-                                <div class="location">
-                                    <a href="properties-details.html">
-                                        <i class="fa fa-map-marker"></i>123 Kathal St. Tampa City
-                                    </a>
-                                </div>
+        @foreach($featuredProperties as $property)
+            @php
+                $unit = $property->units->first();
+                $image = $property->galleries->first();
+            @endphp
+            <div class="wrapper">
+                <div class="card">
+                    <div class="cardImage">
+                        <div class="property-box-4">
+                            <div class="property-photo">
+                                <img class="img-fluid w-100" src="{{ $image ? Storage::url($image->path) : 'https://via.placeholder.com/350x200?text=No+Image' }}" alt="properties">
+                                @if($unit)
+                                    <div class="tag">{{ $unit->availability ? 'For Rent' : 'Not Available' }}</div>
+                                    <div class="plan-price"><sup>$</sup>{{ $unit->rent }}<span>/month</span></div>
+                                @endif
                             </div>
-                            <div class="properties-listing clearfix">
-                                <ul>
-                                    <li><i class="flaticon-bed"></i> 3 Beds</li>
-                                    <li><i class="flaticon-bathroom"></i> 2 Baths</li>
-                                    <li><i class="flaticon-area"></i> 980 sqft</li>
-                                </ul>
+                            <div class="detail">
+                                <div class="heading">
+                                    <h3>
+                                        <a href="#">{{ $property->name }}</a>
+                                    </h3>
+                                    <div class="location">
+                                        <i class="fa fa-map-marker"></i>{{ $property->address }}
+                                    </div>
+                                </div>
+                                @if($unit)
+                                    <div class="properties-listing clearfix">
+                                        <ul>
+                                            <li><i class="flaticon-bed"></i> {{ $unit->bed }} Beds</li>
+                                            <li><i class="flaticon-bathroom"></i> {{ $unit->bath }} Baths</li>
+                                            <li><i class="flaticon-area"></i> {{ $unit->floor_area }} sqft</li>
+                                        </ul>
+                                    </div>
+                                @endif
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="wrapper">
-            <div class="card">
-                <div class="cardImage">
-                    <div class="property-box-4">
-                        <div class="property-photo">
-                            <img class="img-fluid w-100" src="https://storage.googleapis.com/theme-vessel-items/checking-sites/neer-html/HTML/main/img/properties/properties-2.jpg" alt="properties">
-                            <div class="tag">For Rent</div>
-                            <div class="plan-price"><sup>$</sup>540<span>/month</span></div>
-                        </div>
-                        <div class="detail">
-                            <div class="heading">
-                                <h3>
-                                    <a href="properties-details.html">Park avenue</a>
-                                </h3>
-                                <div class="location">
-                                    <a href="properties-details.html">
-                                        <i class="fa fa-map-marker"></i>123 Kathal St. Tampa City
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="properties-listing clearfix">
-                                <ul>
-                                    <li><i class="flaticon-bed"></i> 3 Beds</li>
-                                    <li><i class="flaticon-bathroom"></i> 2 Baths</li>
-                                    <li><i class="flaticon-area"></i> 980 sqft</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="card">
-                <div class="cardImage">
-                    <div class="property-box-4">
-                        <div class="property-photo">
-                            <img class="img-fluid w-100" src="https://storage.googleapis.com/theme-vessel-items/checking-sites/neer-html/HTML/main/img/properties/properties-3.jpg" alt="properties">
-                            <div class="tag">For Sale</div>
-                            <div class="plan-price"><sup>$</sup>820<span>/month</span></div>
-                        </div>
-                        <div class="detail">
-                            <div class="heading">
-                                <h3>
-                                    <a href="properties-details.html">Real Luxury Villa</a>
-                                </h3>
-                                <div class="location">
-                                    <a href="properties-details.html">
-                                        <i class="fa fa-map-marker"></i>123 Kathal St. Tampa City
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="properties-listing clearfix">
-                                <ul>
-                                    <li><i class="flaticon-bed"></i> 3 Beds</li>
-                                    <li><i class="flaticon-bathroom"></i> 2 Baths</li>
-                                    <li><i class="flaticon-area"></i> 980 sqft</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="card">
-                <div class="cardImage">
-                    <div class="property-box-4">
-                        <div class="property-photo">
-                            <img class="img-fluid w-100" src="https://storage.googleapis.com/theme-vessel-items/checking-sites/neer-html/HTML/main/img/properties/properties-4.jpg" alt="properties">
-                            <div class="tag">For Sale</div>
-                            <div class="plan-price"><sup>$</sup>240<span>/month</span></div>
-                        </div>
-                        <div class="detail">
-                            <div class="heading">
-                                <h3>
-                                    <a href="properties-details.html">Relaxing Apartment</a>
-                                </h3>
-                                <div class="location">
-                                    <a href="properties-details.html">
-                                        <i class="fa fa-map-marker"></i>123 Kathal St. Tampa City
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="properties-listing clearfix">
-                                <ul>
-                                    <li><i class="flaticon-bed"></i> 3 Beds</li>
-                                    <li><i class="flaticon-bathroom"></i> 2 Baths</li>
-                                    <li><i class="flaticon-area"></i> 980 sqft</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="card">
-                <div class="cardImage">
-                    <div class="property-box-4">
-                        <div class="property-photo">
-                            <img class="img-fluid w-100" src="https://storage.googleapis.com/theme-vessel-items/checking-sites/neer-html/HTML/main/img/properties/properties-5.jpg" alt="properties">
-                            <div class="tag">For Rent</div>
-                            <div class="plan-price"><sup>$</sup>680<span>/month</span></div>
-                        </div>
-                        <div class="detail">
-                            <div class="heading">
-                                <h3>
-                                    <a href="properties-details.html">Modern Family Home</a>
-                                </h3>
-                                <div class="location">
-                                    <a href="properties-details.html">
-                                        <i class="fa fa-map-marker"></i>123 Kathal St. Tampa City
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="properties-listing clearfix">
-                                <ul>
-                                    <li><i class="flaticon-bed"></i> 3 Beds</li>
-                                    <li><i class="flaticon-bathroom"></i> 2 Baths</li>
-                                    <li><i class="flaticon-area"></i> 980 sqft</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="wrapper">
-            <div class="card">
-                <div class="cardImage">
-                    <div class="property-box-4">
-                        <div class="property-photo">
-                            <img class="img-fluid w-100" src="https://storage.googleapis.com/theme-vessel-items/checking-sites/neer-html/HTML/main/img/properties/properties-6.jpg" alt="properties">
-                            <div class="tag">For Sale</div>
-                            <div class="plan-price"><sup>$</sup>460<span>/month</span></div>
-                        </div>
-                        <div class="detail">
-                            <div class="heading">
-                                <h3>
-                                    <a href="properties-details.html">Luxury Villa</a>
-                                </h3>
-                                <div class="location">
-                                    <a href="properties-details.html">
-                                        <i class="fa fa-map-marker"></i>123 Kathal St. Tampa City
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="properties-listing clearfix">
-                                <ul>
-                                    <li><i class="flaticon-bed"></i> 3 Beds</li>
-                                    <li><i class="flaticon-bathroom"></i> 2 Baths</li>
-                                    <li><i class="flaticon-area"></i> 980 sqft</li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        @endforeach
     </div>
 </div>
 <!-- Featured Properties end -->
